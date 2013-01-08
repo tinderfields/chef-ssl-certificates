45f4321ea8e29774aa07777a61c053347ffa544e
Paul Odeon <mail@paulodeon.com>
2013-01-08 16:52:01 +0000

    Make it work with chef-solo and add ca-bundle

--- definitions/ssl_certificate.rb
+++ definitions/ssl_certificate.rb
@@ -20,38 +20,50 @@
 define :ssl_certificate do
   name = params[:name] =~ /\*\.(.+)/ ? "#{$1}_wildcard" : params[:name]
   Chef::Log.info "Looking for SSL certificate #{name.inspect}"
-  cert = search(:certificates, "name:#{name}").first
+  #cert = search(:certificates, "name:#{name}").first
+  cert = data_bag_item("certificates", name)
   
-  if cert[:crt]
+  if cert['crt']
     template "#{node[:ssl_certificates][:path]}/#{name}.crt" do
       source 'cert.erb'
       owner 'root'
       group 'ssl-cert'
       mode '0640'
       cookbook 'ssl_certificates'
-      variables :cert => cert[:crt]
+      variables :cert => cert['crt']
     end
   end
 
-  if cert[:key]
+  if cert['key']
     template "#{node[:ssl_certificates][:path]}/#{name}.key" do
       source 'cert.erb'
       owner 'root'
       group 'ssl-cert'
       mode '0640'
       cookbook 'ssl_certificates'
-      variables :cert => cert[:key]
+      variables :cert => cert['key']
     end
   end
 
-  if cert[:pem]
+  if cert['pem']
     template "#{node[:ssl_certificates][:path]}/#{name}.pem" do
       source 'cert.erb'
       owner 'root'
       group 'ssl-cert'
       mode '0640'
       cookbook 'ssl_certificates'
-      variables :cert => cert[:pem]
+      variables :cert => cert['pem']
+    end
+  end
+  
+  if cert['ca_bundle']
+    template "#{node[:ssl_certificates][:path]}/#{name}.ca-bundle" do
+      source 'cert.erb'
+      owner 'root'
+      group 'ssl-cert'
+      mode '0640'
+      cookbook 'ssl_certificates'
+      variables :cert => cert['ca_bundle']
     end
   end
 end
